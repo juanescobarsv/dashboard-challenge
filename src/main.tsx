3a781bdd2399092b62ae4583d6c6ca741be38d06
Juan Escobar <juanescobar@ravn.co>
2025-05-22 09:30:06 -0600

    removed src folder to see files

--- src/main.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Dashboard from "./App";
-
-createRoot(document.getElementById("root")!).render(
-    <Dashboard />
-);
